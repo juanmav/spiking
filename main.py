e3b92ae0bf549e5ec6c9bf72734178e9fe0da091
Juan Manuel Vicente <juanma.v82@gmail.com>
2021-06-27 21:08:31 +0200

    Correct "fill" values.

--- main.py
+++ main.py
@@ -204,8 +204,8 @@ proportion = ceil(pattern_image_size / len(get_pattern_0()))
 pattern_image_array_0 = np.kron(np.array(get_pattern_0()), np.ones((proportion, proportion)))
 pattern_image_array_1 = np.kron(np.array(get_pattern_1()), np.ones((proportion, proportion)))
 
-image_array_0 = np.pad(pattern_image_array_0, ceil((total_image_size - pattern_image_size)/2), 'constant', constant_values=0.2)
-image_array_1 = np.pad(pattern_image_array_1, ceil((total_image_size - pattern_image_size)/2), 'constant', constant_values=0.2)
+image_array_0 = np.pad(pattern_image_array_0, ceil((total_image_size - pattern_image_size)/2), 'constant', constant_values=0.0)
+image_array_1 = np.pad(pattern_image_array_1, ceil((total_image_size - pattern_image_size)/2), 'constant', constant_values=0.0)
 
 # Just for small experiments
 diff = len(image_array_0) - total_image_size
